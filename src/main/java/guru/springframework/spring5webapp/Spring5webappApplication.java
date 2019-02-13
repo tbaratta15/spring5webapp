25e130cf646d22014590681f129792afedd8bda0
Tom <thomasbaratta@yahoo.com>
2019-02-13 08:12:28 -0500

    adding JPA example

---
+++ src/main/java/guru/springframework/spring5webapp/Spring5webappApplication.java
@@ -0,0 +1,19 @@
+package guru.springframework.spring5webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5webappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5webappApplication.class, args);
+	}
+
+}
+
+/*
+*This Program uses JPA Mapping using hibernate to create a data schema on localhost 8080/h2-console
+* JDBC Connect on localhost:8080/h2-console/ = jdbc:h2:mem:testdb
+*
+*/
