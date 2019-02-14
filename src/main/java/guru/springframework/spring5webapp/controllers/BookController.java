f2702d00667a921aefbb27138bae471768e60a0f
Tom <thomasbaratta@yahoo.com>
2019-02-14 11:46:37 -0500

    Part 1 finished Spring 5 Book/Author

--- src/main/java/guru/springframework/spring5webapp/controllers/BookController.java
+++ src/main/java/guru/springframework/spring5webapp/controllers/BookController.java
@@ -6,22 +6,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.ui.Model;
 
 /*
- * Spring MVC which will associate with Thymeleaf
+ * Spring MVC Controller which will associate with Thymeleaf and use JPA to pull data from Hibernate
  */
 
+//Book Controller Annotation - now a Spring Bean
 @Controller
 public class BookController {
 
     private BookRepository bookRepository;
 
+    //Constructor
     public BookController(BookRepository bookRepository) {
         this.bookRepository = bookRepository;
     }
-
+    //Request from HTML Thymeleaf
     @RequestMapping("/books")
     public String getBooks(Model model){
 
-        model.addAttribute( "books", bookRepository.findAll());
+        model.addAttribute( "books", bookRepository.findAll()); //find All is an iterator for Thymeleaf
 
         return "books";
 
